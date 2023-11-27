21fbbb4440f211b8791007590ac1d76e5fe29463
Raphael Mello <raphaelpmros@hotmail.com>
2023-11-27 18:40:36 -0300

    Arrumando a identação

--- testes/Economico.php
+++ testes/Economico.php
@@ -1,14 +1,15 @@
 <?php
-session_start();
- $login=false;
-    if($_SESSION["username"]){
-        $login=true;
-        $nomeusuario=$_SESSION["nome"];
+    session_start();
+    $login = false;
+    if ($_SESSION["username"]) {
+        $login = true;
+        $nomeusuario = $_SESSION["nome"];
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,57 +17,57 @@ session_start();
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
     <header id="navbar">
         <nav class="navbar navbar-expand-lg bg-dark">
             <img id="logo" src="./assets/Img_home/logo.jpeg">
             <div class="container-fluid">
-            <a class="navbar-brand" href="#"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="PaginaInicial.php" style="color: white;">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="Reservas.php" style="color: white;">Reservas</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
-                        Categorias
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="Economico.php">Economico</a></li>
-                        <li><a class="dropdown-item" href="Intermediario.php">Intermediario</a></li>
-                        <li><a class="dropdown-item" href="SUV.php">SUV</a></li>
-                        <li><a class="dropdown-item" href="Minivan.php">Minivan</a></li>
-                        <li><a class="dropdown-item" href="Executivo.php">Executivo</a></li>
-                    </ul>
-                </li>
-                <?php 
-                 if(!$login){
-                    ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Login.html" style="color: white;">Login</a>
-                    </li>
-                <?php
-                 }
-                 else{
-                    ?>
+                <a class="navbar-brand" href="#"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                        <a class="nav-link" href="perfil.php" style="color: white;"><?php echo($nomeusuario);?> - Perfil</a>
-                    </li>
-                    <?php
-                 }
-                 ?>
-                </ul>
-                <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-            </div>
+                            <a class="nav-link active" aria-current="page" href="PaginaInicial.php" style="color: white;">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Reservas.php" style="color: white;">Reservas</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
+                                Categorias
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="Economico.php">Economico</a></li>
+                                <li><a class="dropdown-item" href="Intermediario.php">Intermediario</a></li>
+                                <li><a class="dropdown-item" href="SUV.php">SUV</a></li>
+                                <li><a class="dropdown-item" href="Minivan.php">Minivan</a></li>
+                                <li><a class="dropdown-item" href="Executivo.php">Executivo</a></li>
+                            </ul>
+                        </li>
+                        <?php
+                        if (!$login) {
+                        ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="Login.html" style="color: white;">Login</a>
+                            </li>
+                        <?php
+                        } else {
+                        ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="perfil.php" style="color: white;"><?php echo ($nomeusuario); ?> - Perfil</a>
+                            </li>
+                        <?php
+                        }
+                        ?>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                </div>
             </div>
         </nav>
     </header>
@@ -78,7 +79,7 @@ session_start();
             <p>Um carro compacto e econômico para deslocamentos urbanos e viagens curtas.<br> <br> <br>O Volkswagen Polo é conhecido por sua eficiência de combustível e design compacto. Com assentos confortáveis e ótima manobrabilidade, é perfeito para viagens na cidade e passeios curtos. Alugue o Volkswagen Polo hoje e economize enquanto se move com estilo.</p>
             <p>Condições do veículo: Novo</p>
             <p>Combustível: Gasolina</p>
-            <p>Preço Diária: R$85,00 </p>                    
+            <p>Preço Diária: R$85,00 </p>
             <button type="button" id="reserve-button-polo"> <a href="./Reservas.html">Reservar Veículo</a></button>
         </div>
         <div class="vehicle">
@@ -87,7 +88,7 @@ session_start();
             <p>Um hatch compacto e econômico para viagens eficientes e práticas.<br> <br> <br>O Hyundai i20 oferece economia de combustível e um design moderno. Com espaço suficiente para passageiros e bagagem, é uma escolha inteligente para viagens econômicas. Alugue o Hyundai i20 hoje e desfrute da conveniência e da eficiência.</p>
             <p>Condições do veículo: Amassado no porta-malas</p>
             <p>Combustível: Etanol</p>
-            <p>Preço Diária: R$85,00 </p> 
+            <p>Preço Diária: R$85,00 </p>
             <button type="button" id="reserve-button-i20"><a href="./Reservas.html">Reservar Veículo</a></button>
         </div>
         <div class="vehicle">
@@ -97,7 +98,7 @@ session_start();
             <p>Condições do veículo: Novo</p>
             <p>Combustível: Etanol</p>
             <p>Preço Diária: R$85,00 </p>
-                
+
             <button type="button" id="reserve-button-fiesta"><a href="./Reservas.html">Reservar Veículo</a></button>
         </div>
         <div class="vehicle">
@@ -106,34 +107,34 @@ session_start();
             <p>Um carro compacto e econômico para deslocamentos urbanos e viagens eficientes.<br> <br> <br>O Toyota Yaris combina economia de combustível com um design moderno. Seu interior espaçoso e recursos de entretenimento tornam as viagens agradáveis. É a escolha ideal para deslocamentos diários e passeios econômicos. Alugue o Toyota Yaris hoje e experimente a eficiência e o conforto.</p>
             <p>Condições do veículo: Novo</p>
             <p>Combustível: Etanol</p>
-            <p>Preço Diária: R$85,00 </p>                
+            <p>Preço Diária: R$85,00 </p>
             <button type="button" id="reserve-button-yaris"><a href="./Reservas.html">Reservar Veículo</a></button>
         </div>
-            <section id="reservation" class="reservation-container hidden">
-                <h2>Reserva de Veículo</h2>
-                <form id="reservation-form" class="reservation-form">
-                    <label for="pickup-date">Data de Retirada:</label>
-                    <input type="date" id="pickup-date" required>
-                    <label for="return-date">Data de Devolução:</label>
-                    <input type="date" id="return-date" required>
-                    <label for="vehicle-category">Categoria de Veículo:</label>
-                    <select id="vehicle-category" required>
-                        <option value="economico">Econômico</option>
-                        <option value="intermediario">Intermediário</option>
-                        <option value="suv">SUV</option>
-                        <option value="minivan">Minivan</option>
-                        <option value="executivo">Executivo</option>
-                    </select>
-                    <label for="add-options">Opcionais:</label>
-                    <input type="checkbox" id="baby-seat" value="cadeira-bebe">
-                    <label for="baby-seat">Cadeira de Bebê</label>
-                    <input type="checkbox" id="booster-seat" value="direcao-eletrica">
-                    <label for="booster-seat">Direção Elétrica</label>
-                    <input type="checkbox" id="gps" value="gps">
-                    <label for="gps">GPS</label>
-                    <button type="submit">Reservar</button>
-                </form>
-            </section>
+        <section id="reservation" class="reservation-container hidden">
+            <h2>Reserva de Veículo</h2>
+            <form id="reservation-form" class="reservation-form">
+                <label for="pickup-date">Data de Retirada:</label>
+                <input type="date" id="pickup-date" required>
+                <label for="return-date">Data de Devolução:</label>
+                <input type="date" id="return-date" required>
+                <label for="vehicle-category">Categoria de Veículo:</label>
+                <select id="vehicle-category" required>
+                    <option value="economico">Econômico</option>
+                    <option value="intermediario">Intermediário</option>
+                    <option value="suv">SUV</option>
+                    <option value="minivan">Minivan</option>
+                    <option value="executivo">Executivo</option>
+                </select>
+                <label for="add-options">Opcionais:</label>
+                <input type="checkbox" id="baby-seat" value="cadeira-bebe">
+                <label for="baby-seat">Cadeira de Bebê</label>
+                <input type="checkbox" id="booster-seat" value="direcao-eletrica">
+                <label for="booster-seat">Direção Elétrica</label>
+                <input type="checkbox" id="gps" value="gps">
+                <label for="gps">GPS</label>
+                <button type="submit">Reservar</button>
+            </form>
+        </section>
         </div>
     </section>
     <footer>
@@ -142,4 +143,5 @@ session_start();
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="./script.js"></script>
+
 </html>
--- testes/cadastra_cartao.php
+++ testes/cadastra_cartao.php
@@ -1,36 +1,36 @@
 <?php
-ini_set("display_errors", 1);
-require("conecta.php");
-
-$x=file_get_contents('php://input');
-
-$x=json_decode($x);
-
-$num_cartao_cliente=$x->num_cartao_cliente;
-$bandeira_cartao=$x->bandeira_cartao;
-$nome_impresso=$x->nome_impresso;
-$val_cartao_cliente=$x->val_cartao_cliente;
-$cod_seguranca=$x->cod_seguranca;
-
-$sql = "INSERT INTO cartao (NUM_CARTAO, BANDEIRA, NOME_IMPRESSO_CARTAO, VALIDADE_CARTAO, COD_SEGURANCA)
-    VALUES (:par_NUM_CARTAO,:par_BANDEIRA,:par_NOME_IMPRESSO_CARTAO,:par_VALIDADE_CARTAO,:par_COD_SEGURANCA)";
-
-$stmt = $conn->prepare($sql);
-$dados = array(
-    ":par_NUM_CARTAO" => $num_cartao_cliente,
-    ":par_BANDEIRA" => $bandeira_cartao,
-    ":par_NOME_IMPRESSO_CARTAO" => $nome_impresso,
-    ":par_VALIDADE_CARTAO" => $val_cartao_cliente,
-    ":par_COD_SEGURANCA" => $cod_seguranca
-);
-
-$result=$stmt->execute($dados);
-
-if($result){
-  $msg=array("codigo"=>1, "texto"=>"Cadastro realizado com sucesso!");
-} else{
-  $msg=array("codigo"=>0,"texto"=>"Erro ao realizar o cadastro.");
-}
-header('Content-Type:application/json; charset=utf-8');
-echo(json_encode($msg));
+  ini_set("display_errors", 1);
+  require("conecta.php");
+  
+  $x=file_get_contents('php://input');
+  
+  $x=json_decode($x);
+  
+  $num_cartao_cliente=$x->num_cartao_cliente;
+  $bandeira_cartao=$x->bandeira_cartao;
+  $nome_impresso=$x->nome_impresso;
+  $val_cartao_cliente=$x->val_cartao_cliente;
+  $cod_seguranca=$x->cod_seguranca;
+  
+  $sql = "INSERT INTO cartao (NUM_CARTAO, BANDEIRA, NOME_IMPRESSO_CARTAO, VALIDADE_CARTAO, COD_SEGURANCA)
+      VALUES (:par_NUM_CARTAO,:par_BANDEIRA,:par_NOME_IMPRESSO_CARTAO,:par_VALIDADE_CARTAO,:par_COD_SEGURANCA)";
+  
+  $stmt = $conn->prepare($sql);
+  $dados = array(
+      ":par_NUM_CARTAO" => $num_cartao_cliente,
+      ":par_BANDEIRA" => $bandeira_cartao,
+      ":par_NOME_IMPRESSO_CARTAO" => $nome_impresso,
+      ":par_VALIDADE_CARTAO" => $val_cartao_cliente,
+      ":par_COD_SEGURANCA" => $cod_seguranca
+  );
+  
+  $result=$stmt->execute($dados);
+  
+  if($result){
+    $msg=array("codigo"=>1, "texto"=>"Cadastro realizado com sucesso!");
+  } else{
+    $msg=array("codigo"=>0,"texto"=>"Erro ao realizar o cadastro.");
+  }
+  header('Content-Type:application/json; charset=utf-8');
+  echo(json_encode($msg));
 ?>
