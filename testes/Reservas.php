05a7f320ada7a5ea18ae404a7dbc9ad1afcf0ccf
Raphael Mello <raphaelpmros@hotmail.com>
2023-11-28 16:38:11 -0300

    identação

--- testes/Reservas.php
+++ testes/Reservas.php
@@ -109,7 +109,8 @@ if ($_SESSION["username"]) {
                             </li>
                         <?php
                         }
-                        ?>                        </ul>
+                        ?>                        
+                        </ul>
                         <form class="d-flex" role="search">
                             <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
                             <button class="btn btn-outline-success" type="submit">Buscar</button>
