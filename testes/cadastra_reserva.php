75e00dbd86ba4296e742a8cb7fd040f03f763f70
Raphael Mello <raphaelpmros@hotmail.com>
2023-11-28 16:30:18 -0300

    arrumando problema reserva_cadastro

--- testes/cadastra_reserva.php
+++ testes/cadastra_reserva.php
@@ -14,21 +14,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         // Use prepared statements para evitar problemas de SQL injection
         $stmt = $conn->prepare("INSERT INTO reservas (CATEGORIA_VEICULO, DATA_RETIRADA, DATA_DEVOLUCAO, CNH_CLIENTE, CADEIRA_BEBE, ASSENTO_ELEVACAO, GPS)
-            VALUES ($categoria, $data_retirada, $data_devolucao, $cnh_cliente, $cadeira_bebe, $assento_elevacao, $gps)");
+        VALUES (:categoria, :data_retirada, :data_devolucao, :cnh_cliente, :cadeira_bebe, :assento_elevacao, :gps)");
 
         // Ajuste: Atribua as variáveis diretamente a bindParam, sem a necessidade de passar por variáveis intermediárias
-        $stmt->bindParam(1, $categoria);
-        $stmt->bindParam(2, $data_retirada);
-        $stmt->bindParam(3, $data_devolucao);
-        $stmt->bindParam(4, $cnh_cliente);
-        $stmt->bindParam(5, $cadeira_bebe, PDO::PARAM_INT);
-        $stmt->bindParam(6, $assento_elevacao, PDO::PARAM_INT);
-        $stmt->bindParam(7, $gps, PDO::PARAM_INT);
+$stmt->bindParam(':categoria', $categoria);
+$stmt->bindParam(':data_retirada', $data_retirada);
+$stmt->bindParam(':data_devolucao', $data_devolucao);
+$stmt->bindParam(':cnh_cliente', $cnh_cliente);
+$stmt->bindParam(':cadeira_bebe', $cadeira_bebe);
+$stmt->bindParam(':assento_elevacao', $assento_elevacao);
+$stmt->bindParam(':gps', $gps);
 
         $stmt->execute();
 
         echo "<center><h1>Reserva Cadastrada com Sucesso</h1>";
-        echo "<a href='PaginaInicial.html'><input type='button' value='Voltar'></a></center>";
+        echo "<a href='PaginaInicial.php'><input type='button' value='Voltar'></a></center>";
     } catch (PDOException $e) {
         echo "<h3>OCORREU UM ERRO:</h3>: " . $e->getMessage();
     }
